d3ce9115c66f941b8bf19807b7d78ad39d1dc442
Remi Jannel <remi@stripe.com>
2017-03-25 09:18:08 -0400

    Added support for the new Payout and RecipientTransfer objects
    The Transfer object used to represent all movements of funds in Stripe. It
    split in three resources:
    - Transfer: this describes the movement of funds between Stripe accounts
    and is specific to Stripe Connect.
    - Payout: this describes the movement of funds from a Stripe account to a
    bank account, debit card or any future payout method.
    - RecipientTransfer: this describes the movement of funds from a Stripe
    account to a Recipient's card or Bank Account. This is here for legacy
    reasons and can only be accessed from an expanded BalanceTransaction.
    
    This change is behind an API version so old API versions would still use
    the Transfer object for everything while new API version would see the
    split.
    
    This applies beyond the new object as some properties/methods are removed
    from Transfer and other properties are renamed on other objects.

--- tests/TransferTest.php
+++ tests/TransferTest.php
@@ -6,60 +6,22 @@ class TransferTest extends TestCase
 {
     public function testCreate()
     {
-        $recipient = self::createTestRecipient();
+        $transfer = self::createTestTransfer();
 
-        self::authorizeFromEnv();
-        $transfer = Transfer::create(array(
-            'amount' => 100,
-            'currency' => 'usd',
-            'recipient' => $recipient->id
-        ));
-        $this->assertSame('pending', $transfer->status);
+        $this->assertSame('transfer', $transfer->object);
     }
 
     public function testRetrieve()
     {
-        $recipient = self::createTestRecipient();
+        $transfer = self::createTestTransfer();
 
-        self::authorizeFromEnv();
-        $transfer = Transfer::create(array(
-            'amount' => 100,
-            'currency' => 'usd',
-            'recipient' => $recipient->id
-        ));
         $reloaded = Transfer::retrieve($transfer->id);
         $this->assertSame($reloaded->id, $transfer->id);
-    }
-
-    /**
-     * @expectedException Stripe\Error\InvalidRequest
-     */
-    public function testCancel()
-    {
-        $recipient = self::createTestRecipient();
-
-        self::authorizeFromEnv();
-        $transfer = Transfer::create(array(
-            'amount' => 100,
-            'currency' => 'usd',
-            'recipient' => $recipient->id
-        ));
-        $reloaded = Transfer::retrieve($transfer->id);
-        $this->assertSame($reloaded->id, $transfer->id);
-
-        $reloaded->cancel();
     }
 
     public function testTransferUpdateMetadata()
     {
-        $recipient = self::createTestRecipient();
-
-        self::authorizeFromEnv();
-        $transfer = Transfer::create(array(
-            'amount' => 100,
-            'currency' => 'usd',
-            'recipient' => $recipient->id
-        ));
+        $transfer = self::createTestTransfer();
 
         $transfer->metadata['test'] = 'foo bar';
         $transfer->save();
@@ -70,41 +32,12 @@ class TransferTest extends TestCase
 
     public function testTransferUpdateMetadataAll()
     {
-        $recipient = self::createTestRecipient();
-
-        self::authorizeFromEnv();
-        $transfer = Transfer::create(array(
-            'amount' => 100,
-            'currency' => 'usd',
-            'recipient' => $recipient->id
-        ));
+        $transfer = self::createTestTransfer();
 
         $transfer->metadata = array('test' => 'foo bar');
         $transfer->save();
 
         $updatedTransfer = Transfer::retrieve($transfer->id);
         $this->assertSame('foo bar', $updatedTransfer->metadata['test']);
-    }
-
-    public function testRecipientUpdateMetadata()
-    {
-        $recipient = self::createTestRecipient();
-
-        $recipient->metadata['test'] = 'foo bar';
-        $recipient->save();
-
-        $updatedRecipient = Recipient::retrieve($recipient->id);
-        $this->assertSame('foo bar', $updatedRecipient->metadata['test']);
-    }
-
-    public function testRecipientUpdateMetadataAll()
-    {
-        $recipient = self::createTestRecipient();
-
-        $recipient->metadata = array('test' => 'foo bar');
-        $recipient->save();
-
-        $updatedRecipient = Recipient::retrieve($recipient->id);
-        $this->assertSame('foo bar', $updatedRecipient->metadata['test']);
     }
 }
